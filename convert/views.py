d81f91cafc4dddeda7bca7b59622ba17dd8d9725
Vbdvll <konateablaye6@gmail.com>
2025-03-30 22:09:02 +0000

    mise a jour 3

--- convert/views.py
+++ convert/views.py
@@ -5,14 +5,16 @@ def Accueil(request):
     if request.method=="POST":
         nombre=request.POST.get("nombre")
         convType=request.POST.get("conv_type")
-        if(convType=="bin_to_dec"):
-            res=int(nombre,2)
-        elif (convType=="dec_to_bin"):
-            res=bin(int(nombre))
-        else:
-            res="Erreur de conversion"
-        return render(request,"convert/resultat.html",{"res": res})
-    else:
-        render(request,"convert/resultat.html")
+        if nombre is None or convType is None:
+            return render(request,"convert/resultat.html",{"error": "Nombre ou le type de conversion ne peut pas etre nulle"})
+        try :
+            if(convType=="bin_to_dec"):
+                res=int(nombre,2)
+            elif (convType=="dec_to_bin"):
+                res=bin(int(nombre))[2:]
+        except  ValueError:
+            return render(request,"convert/resultat.html",{"error ":"Erreur de conversion"})
+        return render(request,"convert/resultat.html",{"res":res})
+    return render(request,"convert/index.html")
 def resultat(request):
     return render(request,"convert/resultat.html")
